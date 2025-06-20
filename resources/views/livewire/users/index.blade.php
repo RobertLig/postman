2fc2da1e189d9b87fb65b3da00fe51ab8611c568
RobertLig <robertligeza2@gmail.com>
2025-06-20 22:12:51 +0200

    Install and initial config of mcamara localization

--- resources/views/livewire/users/index.blade.php
+++ resources/views/livewire/users/index.blade.php
@@ -70,10 +70,10 @@ new class extends Component {
     <!-- HEADER -->
     <x-header title="Hello" separator progress-indicator>
         <x-slot:middle class="!justify-end">
-            <x-input placeholder="Search..." wire:model.live.debounce="search" clearable icon="o-magnifying-glass" />
+            <x-input placeholder="{{ __('Search...') }}" wire:model.live.debounce="search" clearable icon="o-magnifying-glass" />
         </x-slot:middle>
         <x-slot:actions>
-            <x-button label="Filters" @click="$wire.drawer = true" responsive icon="o-funnel" />
+            <x-button label="{{ __('Filters') }}" @click="$wire.drawer = true" responsive icon="o-funnel" />
         </x-slot:actions>
     </x-header>
 
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
 use Livewire\Volt\Volt;
 
-Volt::route('/', 'users.index')->name('home');
-Volt::route('/register', 'auth.register')->name('register');
-Volt::route('/login', 'auth.login')->name('login');
+Route::group(['prefix' => \Mcamara\LaravelLocalization\Facades\LaravelLocalization::setLocale(),
+                          'middleware' => [ 'localeSessionRedirect', 'localeCookieRedirect', 'localize' ]], function()
+{
+   Volt::route('/', 'users.index')->name('home');
+   Volt::route('/register', 'auth.register')->name('register');
+   Volt::route('/login', 'auth.login')->name('login');
+});
