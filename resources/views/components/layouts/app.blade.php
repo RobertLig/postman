828470fa5e3d5dcf80fb2d78c538cd0d50125dac
RobertLig <robertligeza2@gmail.com>
2025-06-24 09:43:06 +0200

    Start create dropdown-select-reload component

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -33,15 +33,23 @@
  
         {{-- Right side actions --}}
         <x-slot:actions>
-            <x-dropdown label="{{ \Mcamara\LaravelLocalization\Facades\LaravelLocalization::getCurrentLocale() }}" class="btn-sm">
-                @foreach(\Mcamara\LaravelLocalization\Facades\LaravelLocalization::getSupportedLocales() as $localeCode => $properties)
-                    <li>
-                        <a rel="alternate" hreflang="{{ $localeCode }}" href="{{ \Mcamara\LaravelLocalization\Facades\LaravelLocalization::getLocalizedURL($localeCode, null, [], true) }}">
-                            {{ $properties['native'] }}
-                        </a>
-                    </li>
-                @endforeach
-            </x-dropdown>
+            <details class="dropdown" x-data="{open: false}" @click.outside="open = false" :open="open">
+                <summary class="btn btn-sm" @click.prevent="open = !open">
+                    {{ \Mcamara\LaravelLocalization\Facades\LaravelLocalization::getCurrentLocale() }}
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="size-5">
+                        <path fill-rule="evenodd" d="M5.22 8.22a.75.75 0 0 1 1.06 0L10 11.94l3.72-3.72a.75.75 0 1 1 1.06 1.06l-4.25 4.25a.75.75 0 0 1-1.06 0L5.22 9.28a.75.75 0 0 1 0-1.06Z" clip-rule="evenodd" />
+                    </svg>
+                </summary>
+                <ul class="menu dropdown-content bg-base-100 rounded-box z-1 p-2 shadow-sm" @click="open = false">
+                    @foreach(\Mcamara\LaravelLocalization\Facades\LaravelLocalization::getSupportedLocales() as $localeCode => $properties)
+                        <li>
+                            <a rel="alternate" hreflang="{{ $localeCode }}" href="{{ \Mcamara\LaravelLocalization\Facades\LaravelLocalization::getLocalizedURL($localeCode, null, [], true) }}">
+                                {{ $properties['native'] }}
+                            </a>
+                        </li>
+                    @endforeach
+                </ul>
+            </details>
             <x-button label="{{ __('Register') }}" icon="o-envelope" link="{{ \Mcamara\LaravelLocalization\Facades\LaravelLocalization::localizeUrl('/register') }}" 
                 class="btn-ghost btn-sm" responsive 
                 @class(["btn-ghost btn-sm", 
