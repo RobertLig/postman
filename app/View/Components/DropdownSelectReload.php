e78fe32515e070ebc86d8650442d390c63bed5be
RobertLig <robertligeza2@gmail.com>
2025-06-24 14:25:38 +0200

    Create dropdown-select-reload tag

--- app/View/Components/DropdownSelectReload.php
+++ app/View/Components/DropdownSelectReload.php
@@ -8,12 +8,17 @@ use Illuminate\View\Component;
 
 class DropdownSelectReload extends Component
 {
+    public string $uuid;
     /**
      * Create a new component instance.
      */
-    public function __construct()
-    {
-        //
+    public function __construct(
+        public ?string $id = null,
+        public ?string $label = null,
+        // named slots
+        public mixed $icon = null
+    ){
+        $this->uuid = "mary" . md5(serialize($this)) . $id;
     }
 
     /**
@@ -22,9 +27,17 @@ class DropdownSelectReload extends Component
     public function render(): View|Closure|string
     {
         return <<<'blade'
-            <div>
-                <!-- Life is available only in the present moment. - Thich Nhat Hanh -->
-            </div>
+            <details class="dropdown" x-data="{open: false}" @click.outside="open = false" :open="open">
+                <summary {{ $attributes->class(["btn"]) }} @click.prevent="open = !open">
+                    {{ $label }} 
+                    {{ $icon }}
+                </summary>
+                <ul class="menu dropdown-content bg-base-100 rounded-box z-1 p-2 shadow-sm" @click="open = false">
+                    <div wire:key="dropdown-slot-{{ $uuid }}">
+                        {{ $slot }}
+                    </div>
+                </ul>
+            </details>
         blade;
     }
 }
