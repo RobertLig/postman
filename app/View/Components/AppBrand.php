e9d51914672dc2e17c2f5caa9fc68309bce9b667
RobertLig <robertligeza2@gmail.com>
2025-06-27 09:22:24 +0200

    Make app-logo.blade.php component with default attributes

--- app/View/Components/AppBrand.php
+++ app/View/Components/AppBrand.php
@@ -27,7 +27,7 @@ class AppBrand extends Component
                     <div {{ $attributes->class(["hidden-when-collapsed"]) }}>
                         <div class="flex items-center gap-2 w-fit">
                             <!-- <x-icon name="o-cube" class="w-6 -mb-1.5 text-purple-500" /> -->
-                            <x-icons.app-logo class="" />
+                            <x-icons.app-logo width="48" height="48" /> 
                             <span class="font-bold text-3xl bg-clip-text"> <!-- me-3 bg-gradient-to-r from-purple-500 to-pink-300 bg-clip-text text-transparent -->
                                 Postman
                             </span>
