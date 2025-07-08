e439cf3eef64661011a2b172246129480e67d5de
RobertLig <robertligeza2@gmail.com>
2025-07-08 16:56:12 +0200

    Create link-expired view

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Routing\Exceptions\InvalidSignatureException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -21,5 +22,7 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        $exceptions->render(function (InvalidSignatureException $e) {
+            return response()->view('errors.link-expired', status: 403);
+        });
     })->create();
---
+++ resources/views/errors/link-expired.blade.php
@@ -0,0 +1,3 @@
+<div>
+    Expired
+</div>
