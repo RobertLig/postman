dbcc7f241ac19b4b9556ca7d5d0be82ffe4472f5
RobertLig <robertligeza2@gmail.com>
2025-07-09 13:24:47 +0200

    Create 429.blade.php too many request custom file

--- resources/views/errors/429.blade.php
+++ resources/views/errors/429.blade.php
@@ -7,13 +7,14 @@
     justify-content: center;
     align-items: center;
     font-size: 1.25rem;
+    line-height: 1.85rem;
     font-family: sans-serif;
     font-weight: 600;
     background-color: oklch(98% 0.016 73.684);
     color: oklch(40% 0.123 38.172);
     ">
-        <p>{{ __('The link is broken.') }}</p>
-        <p style="margin-top: 0px;">{{ __('Please provide a different one.') }}</p>
+        <p>{{ __('Sorry, you are making too many requests to our servers.') }}</p>
+        <p style="margin-top: 0px;">{{ __('Please wait a few minutes and try again.') }}</p>
 </div>
 
 
