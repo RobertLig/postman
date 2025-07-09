7c615a807b270a69ace0b58a96ebc0c58c5024dc
RobertLig <robertligeza2@gmail.com>
2025-07-09 11:01:27 +0200

    Custom errors.link-expired exception

--- resources/views/errors/link-expired.blade.php
+++ resources/views/errors/link-expired.blade.php
@@ -1,3 +1,20 @@
-<div>
-    Expired
+
+<div style="
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    font-size: 1.25rem;
+    font-family: sans-serif;
+    background-color: oklch(98% 0.016 73.684);
+    color: oklch(40% 0.123 38.172);
+    ">
+        <p>{{ __('The link is broken.') }}</p>
+        <p style="margin-top: 0px;">{{ __('Please provide a different one.') }}</p>
 </div>
+
+
+
+
