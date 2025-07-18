5c607c69965c4d0a3dc8a4865c334153a809c78a
RobertLig <robertligeza2@gmail.com>
2025-07-18 14:59:11 +0200

    Create delete avatar functionality

--- resources/views/livewire/settings/avatar-gender-age.blade.php
+++ resources/views/livewire/settings/avatar-gender-age.blade.php
@@ -1,9 +1,21 @@
 <?php
 
 use Livewire\Volt\Component;
+use Livewire\WithFileUploads;
 
 new class extends Component {
-    //public $photo;
+    use WithFileUploads;
+
+    public $photo;
+
+    public function resetAvatar()
+    {
+        $this->photo = '';
+
+        //$this->js('onAvatarReset'); 
+
+        $this->dispatch('avatar-reset', url: Storage::url('avatars/empty-user.jpg') ); 
+    }
 }; ?>
 
 <div>
@@ -15,13 +27,35 @@ new class extends Component {
 
     <x-form wire:submit="updateProfile" no-separator>
         <div class="grid gap-15 sm:grid-cols-2 sm:gap-5 xl:grid-cols-3 max-w-2xl">
-            <x-file wire:model="photo" accept="image/png, image/jpeg"> 
-                <img src="{{ $user->avatar ?? Storage::url('avatars/empty-user.jpg') }}" class="h-40 rounded-lg" />
-            </x-file> 
+            <div>
+                <x-file wire:model="photo" accept="image/png, image/jpeg" change-text="{{ __('Change') }}"> 
+                    <img src="{{ $user->avatar ?? Storage::url('avatars/empty-user.jpg') }}" class="h-40 rounded-lg" />
+                </x-file> 
+                {{-- @if($photo != '') --}} {{-- wire:click="resetAvatar" --}}
+                    <div x-data="{}">
+                    <x-button  x-on:click="$wire.photo = ''; document.querySelector('div[x-ref] img').src = '{{ Storage::url('avatars/empty-user.jpg') }}';" 
+                        icon="o-trash" class="btn-circle btn-ghost" />
+                    </div>
+                {{-- @endif --}}
+            </div>
         </div>
   
         <x-slot:actions>
             <x-button label="{{ __('Save') }}" icon="o-paper-airplane" class="btn-primary" type="submit" spinner="updateProfile" />
         </x-slot:actions>
     </x-form>
+
+    @script
+        <script>
+            $wire.on('avatar-reset', (event) => {
+                document.querySelector('div[x-ref="preview"] img').src = event.url;
+
+                //console.log(event.url);
+            });
+
+            /*$js('onAvatarReset', () => {
+                document.querySelector('div[x-ref="preview"] > img').src = 
+            })*/
+        </script>
+    @endscript
 </div>
