bc2fd499c2a3f5011792f3c08bf43f93f6bdaf9d
RobertLig <robertligeza2@gmail.com>
2025-07-30 13:26:02 +0200

    Add wire:wheel.prevent= to prevent body scrolling

--- app/View/Components/Carousela.php
+++ app/View/Components/Carousela.php
@@ -31,15 +31,14 @@ class Carousela extends Component
                     </x-slot:trigger>
 
                     <div {{ $attributes->class(['h-50 perspective-distant relative']) }} >
-                        <div class="absolute top-17 transform-3d transition-transform duration-1000 ">
+                        <div wire:wheel.prevent="" class="absolute top-17 transform-3d transition-transform duration-1000 " >
                             @php
                                 $items = [0, 340, 320, 300, 280, 260, 240, 220, 200, 180, 160, 140, 120, 100, 80, 60, 40, 20];
                             @endphp
 
                             @for ($i = 0; $i < 18; $i++)
-                                <div class="p-1 bg-amber-300 absolute" style="transform: rotateX({{ $items[$i] }}deg) translateZ(80px)">{{ $items[$i] }}</div>
+                                <div class="p-1 bg-amber-300 absolute" style="transform: rotateX({{ $items[$i] }}deg) translateZ(83px)">{{ $items[$i] }}</div>
                             @endfor 
-
                         </div>
                     </div>
  
