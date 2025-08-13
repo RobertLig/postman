138156c090fa87104c031a532afd75dff4db2dbc
RobertLig <robertligeza2@gmail.com>
2025-08-13 15:09:05 +0200

    Define LanguageSeeder

--- database/seeders/LanguageSeeder.php
+++ database/seeders/LanguageSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Language;
 
 class LanguageSeeder extends Seeder
 {
@@ -12,6 +13,9 @@ class LanguageSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Language::create([
+            'code' => 'pl', 
+            'name' => 'polski'
+        ]);
     }
 }
