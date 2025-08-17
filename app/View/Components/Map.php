cc40118960981afbe7ac43263318e97b0a4f7d32
RobertLig <robertligeza2@gmail.com>
2025-08-17 22:02:12 +0200

    Add mapTypeControlOptions to map constructor

--- app/View/Components/Map.php
+++ app/View/Components/Map.php
@@ -33,12 +33,20 @@ class Map extends Component
                     map = new Map(document.getElementById('map'), {
                         center: { lat: 52.216667, lng: 21.033333 },
                         zoom: 5,
+                        minZoom: 1,
+                        //maxZoom: 20,
+                        mapTypeControl: true,
+                        mapTypeControlOptions: {
+                            //style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
+                            position: google.maps.ControlPosition.LEFT_BOTTOM,
+                        },
+                        gestureHandling: 'cooperative',
                     });
                 }, 
 
                 
             }" >
-                <div id="map" class="h-100 "></div>
+                <div wire:ignore id="map" class="h-100 "></div>
 
                 <script x-init="initMap()">
                     (g=>{var h,a,k,p="The Google Maps JavaScript API",c="google",l="importLibrary",q="__ib__",m=document,b=window;b=b[c]||(b[c]={});var d=b.maps||(b.maps={}),r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(async(f,n)=>{await (a=m.createElement("script"));e.set("libraries",[...r]+"");for(k in g)e.set(k.replace(/[A-Z]/g,t=>"_"+t[0].toLowerCase()),g[k]);e.set("callback",c+".maps."+q);a.src=`https://maps.${c}apis.com/maps/api/js?`+e;d[q]=f;a.onerror=()=>h=n(Error(p+" could not load."));a.nonce=m.querySelector("script[nonce]")?.nonce||"";m.head.append(a)}));d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})({
