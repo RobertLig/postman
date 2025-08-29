4cbd07882f1f612d051642c2aacd91db0affbda6
RobertLig <robertligeza2@gmail.com>
2025-08-29 10:48:02 +0200

    Add SenderAnnouncement::all() to mount() in index.blade.php

--- resources/views/livewire/senders-announcements/index.blade.php
+++ resources/views/livewire/senders-announcements/index.blade.php
@@ -2,10 +2,27 @@
 
 use Livewire\Volt\Component;
 use Livewire\Attributes\Title;
+use App\Models\SenderAnnouncement;
+use App\Models\Language;
 
 new #[Title('Senders` announcements')]
 class extends Component {
-    //
+    //public SenderAnnouncement $senderAnnouncement;
+
+    public $senderAnnouncements;
+
+    public $language;
+
+    public function mount() //SenderAnnouncement $senderAnnouncement
+    {
+        //$this->senderAnnouncement = $senderAnnouncement;
+
+        $this->senderAnnouncements = SenderAnnouncement::all();
+
+        $this->language = Language::where('code', App::currentLocale())->first();
+
+        //dd($this->language->id);
+    }
 }; ?>
 
 <div>
@@ -13,7 +30,7 @@ class extends Component {
 
         <x-slot:actions>
             @if(auth()->user())
-                <x-button label="{{ __('Create a new ad') }}" responsive icon="o-plus" link="{{ route('senders-announcements.create') }}" class="btn btn-primary btn-sm " />
+                <x-button label="{{ __('Create a new ad') }}" responsive icon="o-plus" link="{{ route('senders-announcements.create') }}" class="btn btn-primary" />
             @endif
 
             <x-button label="{{ __('Filters') }}" @click="$wire.drawer = true" responsive icon="o-funnel" />
@@ -21,8 +38,28 @@ class extends Component {
 
     </x-header>
 
-    <div class="grid sm:grid-cols-2 sm:gap-x-5">
-        
+    <div class="grid sm:grid-cols-2 sm:gap-5"> {{--  --}}
+        {{-- {{ dd($senderAnnouncements) }} --}}
+
+        @foreach ($senderAnnouncements as $senderAnnouncement)
+        {{-- $title = $senderAnnouncement->translate(App::currentLocale()); --}}
+        <x-card :title="$senderAnnouncement->translate($language->id)->thing" shadow separator >
+            I am using slots here.
+ 
+            <x-slot:figure>
+                <img src="https://picsum.photos/500/200" />
+            </x-slot:figure>
+
+            <x-slot:menu>
+                <x-button icon="o-share" class="btn-circle btn-sm" />
+                <x-icon name="o-heart" class="cursor-pointer" />
+            </x-slot:menu>
+
+            <x-slot:actions separator>
+                <x-button :label="__('Details')" class="btn-primary" />
+            </x-slot:actions>
+        </x-card>
+        @endforeach 
     </div>
 
     {{-- @if(auth()->user())
