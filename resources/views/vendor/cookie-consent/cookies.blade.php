0782b2145882fee3e0aa9d9cc626588d6e41cc1f
RobertLig <robertligeza2@gmail.com>
2025-11-14 21:36:40 +0100

    Add tailwindcss classes to cookie consent view

--- resources/views/vendor/cookie-consent/cookies.blade.php
+++ resources/views/vendor/cookie-consent/cookies.blade.php
@@ -10,14 +10,14 @@
                     @endif
                 </div>
                 <div class="cookies__actions">
-                    @cookieconsentbutton(action: 'accept.essentials', label: __('cookieConsent::cookies.essentials'), attributes: ['class' => 'cookiesBtn cookiesBtn--essentials'])
-                    @cookieconsentbutton(action: 'accept.all', label: __('cookieConsent::cookies.all'), attributes: ['class' => 'cookiesBtn cookiesBtn--accept'])
+                    @cookieconsentbutton(action: 'accept.essentials', label: __('cookieConsent::cookies.essentials'), attributes: ['class' => ' cookiesBtn--essentials btn btn-info']) {{-- cookiesBtn --}}
+                    @cookieconsentbutton(action: 'accept.all', label: __('cookieConsent::cookies.all'), attributes: ['class' => ' cookiesBtn--accept btn btn-info']) {{-- cookiesBtn --}}
                 </div>
             </div>
         </div>
-            <a href="#cookies-policy-customize" class="cookies__btn cookies__btn--customize">
+            <a href="#cookies-policy-customize" class="cookies__btn cookies__btn--customize !text-base-content">
                 <span>@lang('cookieConsent::cookies.customize')</span>
-                <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                <svg width="20" height="20" viewBox="0 0 20 20" fill="none" stroke="currentColor" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                     <path d="M14.7559 11.9782C15.0814 11.6527 15.0814 11.1251 14.7559 10.7996L10.5893 6.63297C10.433 6.47669 10.221 6.3889 10 6.38889C9.77899 6.38889 9.56703 6.47669 9.41075 6.63297L5.24408 10.7996C4.91864 11.1251 4.91864 11.6527 5.24408 11.9782C5.56951 12.3036 6.09715 12.3036 6.42259 11.9782L10 8.40074L13.5774 11.9782C13.9028 12.3036 14.4305 12.3036 14.7559 11.9782Z" fill="#2C2E30"/>
                 </svg>
             </a>
@@ -29,16 +29,16 @@
                     <div class="cookies__section">
                         <label for="cookies-policy-check-{{ $category->key() }}" class="cookies__category">
                             @if ($category->key() === 'essentials')
-                                <input type="hidden" name="categories[]" value="{{ $category->key() }}" />
-                                <input type="checkbox" name="categories[]" value="{{ $category->key() }}" id="cookies-policy-check-{{ $category->key() }}" checked="checked" disabled="disabled" />
+                                <input type="hidden" name="categories[]" value="{{ $category->key() }}"/>
+                                <input type="checkbox" name="categories[]" value="{{ $category->key() }}" id="cookies-policy-check-{{ $category->key() }}" checked="checked" disabled="disabled"/>
                             @else
                                 <input type="checkbox" name="categories[]" value="{{ $category->key() }}" id="cookies-policy-check-{{ $category->key() }}" />
                             @endif
                             <span class="cookies__box">
-                                <strong class="cookies__label">{{ $category->title }}</strong>
+                                <strong class="cookies__label !text-base-content">{{ $category->title }}</strong>
                             </span>
                             @if($category->description)
-                                <p class="cookies__info">{{ $category->description }}</p>
+                                <p class="cookies__info !text-base-content">{{ $category->description }}</p>
                             @endif
                         </label>
 
@@ -46,21 +46,21 @@
                             <ul class="cookies__definitions">
                                 @foreach($category->getCookies() as $cookie)
                                 <li class="cookies__cookie">
-                                    <p class="cookies__name">{{ $cookie->name }}</p>
-                                    <p class="cookies__duration">{{ Carbon\Carbon::now()->diffForHumans(Carbon\Carbon::now()->addMinutes($cookie->duration), true) }}</p>
+                                    <p class="cookies__name !text-base-content/60">{{ $cookie->name }}</p>
+                                    <p class="cookies__duration !text-base-content/60">{{ Carbon\Carbon::now()->diffForHumans(Carbon\Carbon::now()->addMinutes($cookie->duration), true) }}</p>
                                     @if($cookie->description)
-                                        <p class="cookies__description">{{ $cookie->description }}</p>
+                                        <p class="cookies__description !text-base-content/60">{{ $cookie->description }}</p>
                                     @endif
                                 </li>
                                 @endforeach
                             </ul>
                         </div>
-                        <a href="#cookies-policy-{{ $category->key() }}" class="cookies__details">@lang('cookieConsent::cookies.details.more')</a>
+                        <a href="#cookies-policy-{{ $category->key() }}" class="cookies__details !text-base-content">@lang('cookieConsent::cookies.details.more')</a>
                     </div>
                     @endforeach
                 </div>
                 <div class="cookies__save">
-                    <button type="submit" class="cookiesBtn__link">@lang('cookieConsent::cookies.save')</button>
+                    <button type="submit" class=" btn btn-info w-full">@lang('cookieConsent::cookies.save')</button>
                 </div>
             </form>
         </div>
