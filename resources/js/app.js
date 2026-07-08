5e160ddbb5d3097a3efb025829be2968ccb2d226
Robert Ligeza <robertligeza2@gmail.com>
2026-07-08 16:24:10 +0200

    Fix Leaflet marker icons

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,5 +5,13 @@ import Sortable from "sortablejs";
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+    iconUrl: "/marker-icon.png",
+    iconRetinaUrl: "/marker-icon-2x.png",
+    shadowUrl: "/marker-shadow.png",
+});
+
 window.Sortable = Sortable;
 window.L = L;
