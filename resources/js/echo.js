a402ea8473ad3147063315e9e993eb4ea05fb860
Robert Ligeza <robertligeza2@gmail.com>
2026-07-08 15:31:23 +0200

    Use environment variables for Reverb configuration

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -7,10 +7,10 @@ window.Echo = new Echo({
     broadcaster: "reverb",
     key: import.meta.env.VITE_REVERB_APP_KEY,
 
-    wsHost: "postman.local",
-    wsPort: 443,
-    wssPort: 443,
+    wsHost: import.meta.env.VITE_REVERB_HOST,
+    wsPort: import.meta.env.VITE_REVERB_PORT ?? 80,
+    wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
 
-    forceTLS: true,
+    forceTLS: import.meta.env.VITE_REVERB_SCHEME === "https",
     enabledTransports: ["ws", "wss"],
 });
